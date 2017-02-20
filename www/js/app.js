aea473ca9b66841eede4250ba767163dc8e18bf0
Brian Matney <brianamatney@gmail.com>
2017-02-20 16:23:26 -0600

    fixed storage issue

--- www/js/app.js
+++ www/js/app.js
@@ -8,13 +8,9 @@ var app = angular.module('todo', ['ionic', 'LocalStorageModule']);
 app.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
+
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
 
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
       cordova.plugins.Keyboard.disableScroll(true);
     }
     if(window.StatusBar) {
@@ -35,7 +31,7 @@ app.controller('main', function ($scope, $ionicModal, localStorageService) {
   $scope.tasks = [];
 
   //initialize the task scope with empty object
-  $scope.task = [];
+  $scope.task = {};
 
   //configure the ionic modal before use
   $ionicModal.fromTemplateUrl('new-task-modal.html', {
